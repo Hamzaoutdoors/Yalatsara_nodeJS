0d8b486786aa524bfd5b15da21ac245d1a9aade5
Hamza <ellaouzihamza@gmail.com>
2022-12-30 09:29:52 +0100

    Created agence routes

--- api/routes/agences.js
+++ api/routes/agences.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getAllAgences, createAgence } from "../controllers/agence.js";
+import { getAllAgences, createAgence, updateAgence, getAgence, deleteAgence } from "../controllers/agence.js";
 
 //GET
 
 router.route("/").get(getAllAgences).post(createAgence);
+router.route("/:id").get(getAgence).patch(updateAgence).delete(deleteAgence)
 
 //UPDATE
 //DELETE
