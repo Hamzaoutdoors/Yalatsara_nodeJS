395ee428d70fb023ed8bdc26733f35201840d91f
Hamza <ellaouzihamza@gmail.com>
2022-12-30 09:28:50 +0100

    feat/add controllers

--- api/controllers/agence.js
+++ api/controllers/agence.js
@@ -1,10 +1,22 @@
 import express from "express";
 import Agence from "../models/Agence.js"
 import asyncWrapper from "../middleware/async.js";
+import { createCustomError } from "../errors/custom-error.js"
 
 const router = express.Router();
 
 //GET
+export const getAgence = asyncWrapper(async (req, res, next) => {
+    const { id: agenceID } = req.params;
+    const agence = await Agence.findOne({ _id: agenceID });
+
+    if (!agence) {
+        return next(createCustomError(`No task with id: ${agenceID}`, 404))
+    }
+
+    res.status(200).json({ agence })
+})
+
 //GET ALL
 export const getAllAgences = asyncWrapper(async (req, res) => {
     const agences = await Agence.find({});
@@ -17,5 +29,32 @@ export const createAgence = asyncWrapper(async (req, res) => {
     const agence = await Agence.create(req.body)
     res.status(201).json({ agence })
 })
+
 //UPDATE
+
+export const updateAgence = asyncWrapper(async (req, res, next) => {
+    const { id: agenceId } = req.params;
+
+    const agence = await Agence.findByIdAndUpdate({ _id: agenceId }, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    if (!agence) {
+        return next(createCustomError(`No task with id : ${agenceId}`, 404))
+    }
+
+    res.status(200).json({ agence })
+})
 //DELETE
+
+export const deleteAgence = asyncWrapper(async (req, res, next) => {
+    const { id: agenceID } = req.params;
+    const agence = await Agence.findByIdAndDelete({ _id: agenceID });
+
+    if (!agence) {
+        return next(createCustomError(`No agence with id: ${agenceID}`, 404))
+    }
+
+    res.status(200).json({ agence })
+})
